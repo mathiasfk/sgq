506b325925f70d581433d6be946fa493fd337bdd
Mathias Kriebel <mathias.kriebel@gmail.com>
2020-05-31 18:09:09 -0300

    JWT authentication service
    - swapping fake authentication with real JWT API
    - mongo-db service to store users data

--- services/web/sgq-app/src/index.js
+++ services/web/sgq-app/src/index.js
@@ -17,7 +17,6 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from 'react-redux';
 
